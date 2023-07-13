09fb4e1c674c84170ee8ec1625ad9405c77098fd
Brett Cocking <brett@trademarktechnology.co.za>
2023-07-13 20:00:15 +0200

    Add personal about data

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -8,4 +8,42 @@
   aria-label="About Me"
 >
   <SectionHeader title="About" />
+  <p class="mb-4">
+    It all started over a decade ago when I wanted to remove some padding on a
+    web template I had. Little did I know that that simple act of tinkering
+    would unlock a world of coding to me and set my on a journey in software
+    engineering.
+  </p>
+  <p class="mb-4">
+    Throughout my career, I've had the opporunity to work with a diverse range
+    of clients and companies, from individuals to small businesses and even on
+    my own
+    <a
+      href="https://playup.coach"
+      class="text-slate-900 dark:text-slate-200 font-medium hover:text-sky-400 hover:dark:text-sky-300"
+      target="_blank">startup</a
+    >. I've collaborated on projects spanning various industries, from
+    <a
+      href="https://hoopsprod.co.za"
+      class="text-slate-900 dark:text-slate-200 font-medium hover:text-sky-400 hover:dark:text-sky-300"
+      target="_blank">film production</a
+    >
+    and
+    <a
+      href="https://offerzen.co.za"
+      class="text-slate-900 dark:text-slate-200 font-medium hover:text-sky-400 hover:dark:text-sky-300"
+      target="_blank">recruitment</a
+    >
+    to
+    <a
+      href="https://chaebanicecream.com"
+      class="text-slate-900 dark:text-slate-200 font-medium hover:text-sky-400 hover:dark:text-sky-300"
+      target="_blank">e-commerce</a
+    > and sport.
+  </p>
+  <p>
+    When I'm not working, I'm either on the golf course, following my football
+    team, playing video games or playing around with a new language or
+    framework.
+  </p>
 </section>
