f704a0af12633ed725d8e318492fa5b8d8c0f303
Brett Cocking <brett@cocking.dev>
2026-06-18 13:57:47 +0100

    refactor(data): move nav data to data package

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -3,3 +3,5 @@ export { experience } from "./experience";
 export type { Project, ProjectImageKey } from "./projects";
 export { projects } from "./projects";
 export { links } from "./links";
+export type { NavItem } from "./nav";
+export { navMenu } from "./nav";
---
+++ packages/data/src/nav.ts
@@ -0,0 +1,19 @@
+export interface NavItem {
+  name: string;
+  description: string;
+}
+
+export const navMenu: NavItem[] = [
+  {
+    name: "About",
+    description: "Learn more about me and my work",
+  },
+  {
+    name: "Experience",
+    description: "View my professional experience",
+  },
+  {
+    name: "Projects",
+    description: "View my projects",
+  },
+];
